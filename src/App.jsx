43cc74b6d39748396db55ffe7f05a0e893dc3d29
Eren Ülger <fliegendemann@hotmail.com>
2025-05-05 00:00:36 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useContext, useState } from "react";
+
+import { Link } from "react-router-dom";
+import Signin from "./components/Signin";
+
+import { UserAuth } from "./context/AuthContext";
+
+function App() {
+  const { user } = UserAuth();
+
+  return (
+    <>
+      <Signin />
+    </>
+  );
+}
+
+export default App;
